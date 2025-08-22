90e855dcf59fc6bc3774842141bc107a5be7c968
Reyvelt <163800012+Reyvelt@users.noreply.github.com>
2025-08-23 07:26:22 +0800

    Add nullptr check to create_db_header

--- src/parse.c
+++ src/parse.c
@@ -206,8 +206,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
 
 int create_db_header(struct dbheader_t **headerOut) {
 
-  struct dbheader_t **header_test = NULL;
-  if(sizeof(*headerOut) != sizeof(struct dbheader_t *)) {
+  if(*headerOut == NULL) {
     printf("Invalid Header Address\n");
     return STATUS_ERROR;
   }
